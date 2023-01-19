66902f382a19355de68ae3c527f3a3c9e0eeb00a
hp <movti@mail.ru>
2023-01-19 13:48:38 +0300

    Исправил домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.random.RandomGenerator;
+
 public class Main {
     public static void main(String[] args) {
         // дз1
@@ -6,10 +6,10 @@ public class Main {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
         int count = 0;
-        int j;
+        int i;
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
             count = count + arr[i];
 
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("Сумма трат за месяц составила " + count + " рублей");
         //дз2
         System.out.println("Домашнее задание 2");
-        for (int i = 0; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
 
@@ -31,19 +31,16 @@ public class Main {
         //дз3
         System.out.println("Домашнее задание 3");
         double averageNumber = 0;
-        for (int i = 0; i < arr.length; i++) {
-            j = i + 1;
+        double sum = count;
 
-            double sum = count;
+        averageNumber = sum / i;
 
-            averageNumber = sum / j;
-        }
 
         System.out.println("Средняя сумма трат за месяц составила" + Math.round(averageNumber) + " рублей");
         //дз 4
         System.out.println("Домашнее задание 4 ");
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
 
         }
